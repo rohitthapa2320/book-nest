b6d6404d556def454a810086fccdb1ee0283e43e
rohitthapa2320 <rohitthapa2362000@gmail.com>
2024-07-20 11:07:26 +0530

    Added register api for users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/users";
+
 mongoose.connect(process.env.MONGODB_URI as string);
 
 const app = express();
@@ -10,11 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
-app.get("/api/test", async(req: Request, res: Response) => {
-  res.json({
-    message: "Hello from express server"
-  });
-});
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on 3000")
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,62 @@
+import {  Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+import User from "../models/user";
+
+const router= Router();
+
+
+// /api/users/register
+router.post("/register",[
+  check("email", "Email is required.").isString(),
+  check("firstName", "First Name is required.").isString(),
+  check("lastName", "Last Name is required.").isString(),
+  check("password", "Password with 6 or more characters are required.").isLength({
+    min: 6
+  }),
+], async(req: Request, res: Response) => {
+
+  const errors= validationResult(req);
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      message: errors.array()
+    })
+  }
+  try {
+    let user= await User.findOne({
+      email: req.body.email
+    });
+
+    if(user){
+      return res.status(400).json({
+        message: "User already esists."
+      })
+    }
+
+    user= new User(req.body);
+    await user.save();
+
+    const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET_KEY as string, {
+      expiresIn: "1d"
+    });
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000
+    });
+
+    return res.sendStatus(200);
+  } catch (error) {
+      console.log({
+        error
+      });
+      res.status(500).send({
+        message: "Something went wrong!"
+      })
+  }
+});
+
+export default router;
