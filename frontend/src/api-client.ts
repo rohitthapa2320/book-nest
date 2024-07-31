6dceb4eed7307ae5fed18ff2ba083cf753063647
rohitthapa2320 <rohitthapa2362000@gmail.com>
2024-07-31 21:44:06 +0530

    update env

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -9,7 +9,8 @@ import {
   UserType,
 } from "./types/types";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://hotelhub-com.onrender.com";
 
 export const register = async (formData: RegisterFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/users/register`, {
