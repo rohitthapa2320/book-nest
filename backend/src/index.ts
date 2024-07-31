bab7ce02758a393f434c087770f5d758cdf909d4
rohitthapa2320 <rohitthapa2362000@gmail.com>
2024-08-01 00:09:01 +0530

    fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://hotelhub-id4j.onrender.com/",
     credentials: true,
   })
 );
